8b9b13f1c45b74a28c4c7950528ac129119bb8d7
Aubree <aubreehowley@outlook.com>
2023-04-02 14:56:59 +0800

    Fix created at field

--- entities/createdAt.go
+++ entities/createdAt.go
@@ -9,6 +9,11 @@ type CreatedAt struct {
 	CreatedAt dataTypes.Time `json:"created_at" db_type:"DATETIME" db_disallow_external_modification:"true" parse_struct:"false"`
 }
 
+func (val CreatedAt) OnCreate() (CreatedAt, error) {
+	val.CreatedAt = dataTypes.Time{Time: time.Now()}
+	return val, nil
+}
+
 func (CreatedAt) Describe() {
 
 }
