7c4bdcce184e4139e87ff261d70013ab92b092fa
Aubree <aubreehowley@outlook.com>
2023-06-04 21:40:57 +0800

    Add FromTable for specifying string without entity

--- query/query_types.go
+++ query/query_types.go
@@ -50,12 +50,21 @@ type table struct {
 }
 
 func (t table) Format() (string, error) {
-	tblInfo, err := structParsing.GetTableInfo(t.Entity)
-	if err != nil {
-		return "", err
+	var tableName string
+
+	switch t.Entity.(type) {
+	case string:
+		tableName = t.Entity.(string)
+		break
+	default:
+		tblInfo, err := structParsing.GetTableInfo(t.Entity)
+		if err != nil {
+			return "", err
+		}
+		tableName = tblInfo.Name
 	}
 
-	return tblInfo.Name + " " + t.Alias, nil
+	return tableName + " " + t.Alias, nil
 }
 
 type parameter struct {
